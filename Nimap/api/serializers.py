5bd27db1c7dec54492b6d4d3c38c335136179d00
Akshay AI <127962935+akshaywaghmode2099@users.noreply.github.com>
2024-03-23 23:09:56 +0530

    Update serializers.py
    update

--- Nimap/api/serializers.py
+++ Nimap/api/serializers.py
@@ -29,44 +29,31 @@ class ClientwithallSerializer(serializers.ModelSerializer):
 
 
 
-class ProjectSerializer2(serializers.ModelSerializer):
-    users = UserSerializer(many=True, read_only=True)
-    # client = ClientSerializer(source='project', read_only=True)
-    class Meta:
-        model = Project
-        fields = ['id', 'project_name','client', 'users','created_at', 'created_by']   #'client', 'users', 
-
-
-class ProjectSerializer2(serializers.ModelSerializer):
-    users = UserSerializer(many=True, read_only=True)
-    # client = ClientSerializer(source='project', read_only=True)
-    class Meta:
-        model = Project
-        fields = ['id', 'project_name','client', 'users','created_at', 'created_by']
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      
-   
+# class ProjectSerializer2(serializers.ModelSerializer):
+#     users = UserSerializer(many=True, read_only=True)
+#     # client = ClientSerializer(source='project', read_only=True)
+#     class Meta:
+#         model = Project
+#         fields = ['id', 'project_name','client', 'users','created_at', 'created_by']  
         
 
+class ProjectSerializer2(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ['id', 'project_name', 'client', 'users', 'created_at', 'created_by']
 
-    
-    
+    def create(self, validated_data):
+        """
+        Custom create method to handle nested relationships (users)
+        """
+        users_data = validated_data.pop('users', [])  
+        project = Project.objects.create(**validated_data)  
+
+        # Add users to the project
+        for user_data in users_data:
+            user_id = user_data.get('id')
+            if user_id:  
+                user = User.objects.get(pk=user_id)
+                project.users.add(user)
+
+        return project
